8a75cc326d41fbab4f776aa1f52fbab2b3403ad3
pclsy <418383134@qq.com>
2016-08-24 19:00:55 +0800

    Default

---
+++ src/main/Test.java
@@ -0,0 +1,26 @@
+package main;
+
+/**
+ * Created by pclsy on 2016/8/24.
+ */
+public class Test {
+    private  int a ;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public Test(int a) {
+        this.a = a;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World Hello");
+
+
+    }
+}
