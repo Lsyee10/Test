e1c461c09cc53ad5b7e48fa4fe9534bfc0fe3196
pclsy <418383134@qq.com>
2016-08-25 15:48:20 +0800

    Default

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -4,10 +4,10 @@ package main;
  * Created by pclsy on 2016/8/25.
  */
 public class Test2 {
-    private  Test test = new Test(1);
+    private Test test = new Test(1);
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(123);
     }
 
 }
